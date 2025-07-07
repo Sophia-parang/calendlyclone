3014f05f55d9e06e18978550d4b7a12546812e31
Sophia Na (나소연) <devsophia029@gmail.com>
2025-07-07 18:26:00 +0900

    feat: add login and register pages

---
+++ app/(auth)/register/[[...register]]/page.tsx
@@ -0,0 +1,13 @@
+import { SignUp } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function RegisterPage() {
+  return (
+    <main className="flex flex-col items-center p-5 gap-10 animate-fade-in">
+      <Image src="/asset/logo.svg" width={100} height={100} alt="Logo" />
+      <div>
+        <SignUp />
+      </div>
+    </main>
+  );
+}
